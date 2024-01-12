65f0a772fc793207dc26934e44e5d3f3767fbbb4
WendelGregorio <contact.wendelg@gmail.com>
2024-01-12 10:59:35 -0300

    fix json await 2

--- src/Hooks/useFetch.jsx
+++ src/Hooks/useFetch.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function useFetch() {
+const useFetch = () => {
   const [data, setData] = React.useState(null)
   const [error, setError] = React.useState(null)
   const [loading, setLoading] = React.useState(false)
@@ -8,7 +8,6 @@ function useFetch() {
   const request = React.useCallback(async (url, options) => {
     let response
     let json
-
     try {
       setError(null)
       setLoading(true)
@@ -26,7 +25,12 @@ function useFetch() {
     }
   }, [])
 
-  return { data, loading, error, request }
+  return {
+    data,
+    loading,
+    error,
+    request,
+  }
 }
 
 export default useFetch

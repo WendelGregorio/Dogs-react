275d30d8f81360ba1ddd418fd745b32613db1e7e
WendelGregorio <contact.wendelg@gmail.com>
2024-01-12 13:41:47 -0300

    fix comment textarea

--- src/Components/Photo/PhotoContent.jsx
+++ src/Components/Photo/PhotoContent.jsx
@@ -14,24 +14,24 @@ function PhotoContent({ data }) {
     <div className={styles.photo}>
       <div className={styles.img}>
         <Image src={photo.src} alt={photo.title} />
-        <div className={styles.details}>
-          <div>
-            <p className={styles.author}>
-              {user.data && user.data.username === photo.author ? (
-                <PhotoDelete id={photo.id} />
-              ) : (
-                <Link to={`/perfil/${photo.author}`}>@{photo.autho}</Link>
-              )}
-              <span className={styles.visualizacoes}>{photo.acessos}</span>
-              <h1 className="title">
-                <Link to={`/foto/${photo.id}`}>{photo.title}</Link>
-              </h1>
-              <ul className={styles.attributes}>
-                <li>{photo.peso} kg</li>
-                <li>{photo.idade} anos</li>
-              </ul>
-            </p>
-          </div>
+      </div>
+      <div className={styles.details}>
+        <div>
+          <p className={styles.author}>
+            {user.data && user.data.username === photo.author ? (
+              <PhotoDelete id={photo.id} />
+            ) : (
+              <Link to={`/perfil/${photo.author}`}>@{photo.autho}</Link>
+            )}
+            <span className={styles.visualizacoes}>{photo.acessos}</span>
+            <h1 className="title">
+              <Link to={`/foto/${photo.id}`}>{photo.title}</Link>
+            </h1>
+            <ul className={styles.attributes}>
+              <li>{photo.peso} kg</li>
+              <li>{photo.idade} anos</li>
+            </ul>
+          </p>
         </div>
       </div>
       <PhotoComments id={photo.id} comments={comments} />

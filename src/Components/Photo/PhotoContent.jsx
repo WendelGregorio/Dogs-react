fd93b8cf6ebc395e4331eb9f9e676fe054212369
WendelGregorio <contact.wendelg@gmail.com>
2024-01-12 11:13:19 -0300

    fix base vite 2

--- src/Components/Photo/PhotoContent.jsx
+++ src/Components/Photo/PhotoContent.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 import styles from './PhotoContent.module.css'
 import PhotoComments from './PhotoComments'
 import { Link } from 'react-router-dom'
+import { UserContext } from '../../Context/UserContext'
+import PhotoDelete from './PhotoDelete'
 
 function PhotoContent({ data }) {
+  const user = React.useContext(UserContext)
   const { photo, comments } = data
+
   return (
     <div className={styles.photo}>
       <div className={styles.img}>
@@ -12,7 +16,11 @@ function PhotoContent({ data }) {
         <div className={styles.details}>
           <div>
             <p className={styles.author}>
-              <Link to={`/perfil/${photo.author}`}>@{photo.autho}</Link>
+              {user.data && user.data.username === photo.author ? (
+                <PhotoDelete id={photo.id} />
+              ) : (
+                <Link to={`/perfil/${photo.author}`}>@{photo.autho}</Link>
+              )}
               <span className={styles.visualizacoes}>{photo.acessos}</span>
               <h1 className="title">
                 <Link to={`/foto/${photo.id}`}>{photo.title}</Link>

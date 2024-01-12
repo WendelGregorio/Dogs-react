32822c65a09540b275b75c77ae9537917a520b97
WendelGregorio <contact.wendelg@gmail.com>
2024-01-12 10:27:51 -0300

    Feat: comentários

--- src/Components/Photo/PhotoComments.jsx
+++ src/Components/Photo/PhotoComments.jsx
@@ -5,11 +5,16 @@ import styles from './PhotoComments.module.css'
 
 function PhotoComments(props) {
   const [comments, setComments] = React.useState(() => props.comments)
+  const commentsSection = React.useRef(null)
   const { login } = React.useContext(UserContext)
+
+  React.useEffect(() => {
+    commentsSection.current.scrollTop = commentsSection.current.scrollHeight
+  }, [comments])
 
   return (
     <>
-      <ul className={styles.comment}>
+      <ul ref={commentsSection} className={styles.comment}>
         {comments.map((comment) => (
           <li key={comment.id}>
             <b>{comment.comment_author}: </b>

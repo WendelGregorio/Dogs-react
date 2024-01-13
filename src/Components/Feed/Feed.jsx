de3e773ff3af6c2e32d41ffb35748c4e18dd06cf
WendelGregorio <contact.wendelg@gmail.com>
2024-01-13 08:39:55 -0300

    feat: PropTypes on feed

--- src/Components/Feed/Feed.jsx
+++ src/Components/Feed/Feed.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import FeedModal from './FeedModal'
 import FeedPhotos from './FeedPhotos'
+import PropTypes from 'prop-types'
 
 function Feed({ user }) {
   const [modalPhoto, setModalPhoto] = React.useState(null)
@@ -49,6 +50,17 @@ function Feed({ user }) {
       ))}
     </div>
   )
+}
+
+Feed.defaultProps = {
+  user: 0,
+}
+
+Feed.PropTypes = {
+  user: PropTypes.oneOfType([
+    PropTypes.string.isRequired,
+    PropTypes.number.isRequired,
+  ]),
 }
 
 export default Feed

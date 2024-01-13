81d2f14752bc04e3b6dd3dc67ddbd7f9b61f190f
WendelGregorio <contact.wendelg@gmail.com>
2024-01-13 09:44:18 -0300

    feat: graphs of user stats

--- src/Components/User/UserStats.jsx
+++ src/Components/User/UserStats.jsx
@@ -1,7 +1,31 @@
 import React from 'react'
+import useFetch from '../../Hooks/useFetch'
+import { STATS_GET } from '../../api/api'
+import Loading from '../Helper/Loading'
+import Error from '../Helper/Error'
+import UserStatsGraphs from './UserStatsGraphs'
 
 function UserStats() {
-  return <div>Estatísticas</div>
+  const { data, error, loading, request } = useFetch()
+
+  React.useEffect(() => {
+    async function getData() {
+      const { url, options } = STATS_GET()
+      await request(url, options)
+    }
+
+    getData()
+  }, [request])
+
+  if (loading) <Loading />
+  if (error) <Error error={error} />
+  if (data)
+    return (
+      <div>
+        <UserStatsGraphs data={data} />
+      </div>
+    )
+  else return null
 }
 
 export default UserStats

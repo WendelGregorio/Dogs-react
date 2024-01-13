3e0694b0b52014c484bcc1d461b9debfa404860a
WendelGregorio <contact.wendelg@gmail.com>
2024-01-13 10:01:29 -0300

    fixing routes

--- src/Components/User/User.jsx
+++ src/Components/User/User.jsx
@@ -14,9 +14,9 @@ function User() {
     <section className="container">
       <UserHeader />
       <Routes>
-        <Route path="/" element={<Feed user={data.id} />} />
-        <Route path="postar" element={<UserPhotoPost />} />
-        <Route path="estatisticas" element={<UserStats />} />
+        <Route path="/Dogs-react/" element={<Feed user={data.id} />} />
+        <Route path="/Dogs-react/postar" element={<UserPhotoPost />} />
+        <Route path="/Dogs-react/estatisticas" element={<UserStats />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </section>
